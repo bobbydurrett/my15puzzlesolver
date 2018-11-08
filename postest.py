bd8d5fd939d46e817b7eb06ba3226151212724a8
Bobby Durrett <bobby@bobbydurrettdba.com>
2018-11-08 16:27:07 -0700

    Added priority queue to postest

--- postest.py
+++ postest.py
@@ -1,22 +1,40 @@
 """
 
 Creates a bunch of Position objects to see 
-how fast they eat up memory.
+how fast they eat up memory. Puts them in
+priority queue to include its memory use.
 
 """
 
 from astar import *
+import time
 
+print(" ")
 positions_created = int(input("Number of positions to create? "))
 
-poslist = []
+before = time.perf_counter()
+
+poslist = PriorityQueue([])
 for i in range(positions_created):
-    poslist.append(
+    poslist.push(
         Position([[ 15, 14,  1, 6],
                   [ 9, 11,  4, 12],
                   [ 0, 10,  7,  3],
                   [13,  8,  5,  2]]))
+                  
+after = time.perf_counter()
 
+print(" ")
 print(str(positions_created)+" successfully created.")
+print(" ")
+print("Run time in seconds: "+str(after - before))
+print(" ")
+
 
 x = input("Hit enter to continue: ")
+
+
+
+
+
+
