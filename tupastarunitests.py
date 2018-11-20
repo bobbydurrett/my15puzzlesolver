546144210d915c204f047bd707a23c628db3f818
bobbydurrett <bobby@bobbydurrettdba.com>
2018-11-20 16:35:00 -0700

    some unit tests

--- tupastarunitests.py
+++ tupastarunitests.py
@@ -129,3 +129,107 @@ moves30 = Position([[ 9,  3,  4,  8],
 
 print(hob.heuristic(moves30))
 
+# one move
+
+start = Position([[ 1,  2,  3,  4],
+                 [ 5,  6,  7,  8],
+                 [ 9, 10, 11, 12],
+                 [13, 14, 0,  15]])
+
+
+
+goal = Position([[ 1,  2,  3,  4],
+                 [ 5,  6,  7,  8],
+                 [ 9, 10, 11, 12],
+                 [13, 14, 15,  0]])
+                 
+result = a_star(start,goal)
+
+print(start)
+
+print(path_as_0_moves(result))
+
+def do_move(goal,direction):
+    """
+    board is 4x4 list of lists with
+    0,0 as top left.
+    
+    board[y][x]
+    
+    direction is r,l,u,d
+    
+    returns updated board
+    """
+    
+    board = goal.copy_tiles()
+
+    # find 0 - blank square
+    
+    x0 = None
+    y0 = None
+    
+    for i in range(4):
+        for j in range(4):
+            if board[i][j] == 0:
+                y0 = i
+                x0 = j
+                
+    if x0 == None or y0 == None:
+        return goal
+        
+    if direction == 'r':
+        # move 0 to the right
+        if x0 < 3:
+            temp = board[y0][x0+1]
+            board[y0][x0+1] = 0
+            board[y0][x0] = temp
+    elif direction == 'l':
+        # move 0 to the left
+        if x0 > 0:
+            temp = board[y0][x0-1]
+            board[y0][x0-1] = 0
+            board[y0][x0] = temp
+    elif direction == 'u':
+        # move 0 up
+        if y0 > 0:
+            temp = board[y0-1][x0]
+            board[y0-1][x0] = 0
+            board[y0][x0] = temp
+    elif direction == 'd':
+        # move 0 down
+        if y0 < 3:
+            temp = board[y0+1][x0]
+            board[y0+1][x0] = 0
+            board[y0][x0] = temp
+    else:
+        print("Bad direction: "+direction)
+    
+    return Position(board)
+    
+# 30 moves
+
+
+start = Position([[ 9,  3,  4,  8],
+                 [  5,  1, 12,  0],
+                 [  6, 10, 11,  2],
+                 [ 13, 14, 15,  7]])
+
+
+goal = Position([[ 1,  2,  3,  4],
+                 [ 5,  6,  7,  8],
+                 [ 9, 10, 11, 12],
+                 [13, 14, 15,  0]])
+                 
+result = a_star(start,goal)
+
+p = path_as_0_moves(result)
+
+current = start
+
+print(start)
+
+for m in p:
+    print(m)
+    current = do_move(current,m)
+    print(current)
+    
